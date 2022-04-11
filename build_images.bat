d2f5eedcc18bc68f79b0899cebe90cd6b96d7148
Geoffrey Cheung <kahogeoff@gmail.com>
2022-04-12 04:28:42 +0800

    [Init] Initial commit for the main repo

---
+++ build_images.bat
@@ -0,0 +1,2 @@
+docker build --file=./frontend/Dockerfile -t geoff4321/quiz-game-frontend .
+docker build --file=./backend/Dockerfile -t geoff4321/quiz-game-backend .
---
+++ save_images.bat
@@ -0,0 +1,2 @@
+docker save geoff4321/quiz-game-frontend --output geoff4321-quiz-game-frontend.tar
+docker save geoff4321/quiz-game-backend --output geoff4321-quiz-game-backend.tar
